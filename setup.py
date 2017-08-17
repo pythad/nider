673596992c061676485b5d43d2df37140a53fd0d
Vladyslav Ovchynnykov <ovd4mail@gmail.com>
2017-08-17 15:49:37 +0300

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test_requirements = [
 
 setup(
     name='nider',
-    version='0.3.3',
+    version='0.3.4',
     description="Python package to add text to images, textures and different backgrounds",
     long_description=readme + '\n\n' + history,
     author="Vladyslav Ovchynnykov",
